6b37ec1af5b7ed17b78bca802ee3f9a1b49f59ed
Spaghet-Ti <spaghet-ti@hotmail.com>
2018-10-31 22:07:21 -0400

    10/31 Progress
    Basic setup

--- main.js
+++ main.js
@@ -1,30 +1,125 @@
 "use strict";
 
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 
 require("electron-reload")(__dirname);
 
 // Keep a global reference of the window object, if you don"t, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+let win;
 
 function createWindow() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({ width: 800, height: 600 });
+    win = new BrowserWindow({ show: false, backgroundColor: "#CCCCCC" });
+    win.maximize();
+    win.show();
 
     // and load the index.html of the app.
-    mainWindow.loadFile("index.html");
+    win.loadFile("index.html");
+
+    var menu = Menu.buildFromTemplate([
+        {
+            label: "File",
+            submenu: [
+                {
+                    label: "New",
+                    click() {},
+                    accelerator: "CmdOrCtrl+N"
+                },
+                {
+                    label: "Open",
+                    click() {},
+                    accelerator: "CmdOrCtrl+O"
+                },
+                { type: "separator" },
+                {
+                    label: "Save",
+                    click() {},
+                    accelerator: "CmdOrCtrl+S"
+                },
+                {
+                    label: "Save As",
+                    click() {},
+                    accelerator: "CmdOrCtrl+Shift+S"
+                },
+                { type: "separator" },
+                {
+                    label: "Print",
+                    click() {},
+                    accelerator: "CmdOrCtrl+P"
+                },
+                { type: "separator" },
+                {
+                    role: "quit"
+                }
+            ]
+        },
+        {
+            label: "Edit",
+            submenu: [
+                {
+                    role: "undo",
+                    accelerator: "CmdOrCtrl+Z"
+                },
+                {
+                    role: "redo",
+                    accelerator: "CmdOrCtrl+Y"
+                },
+                { type: "separator" },
+                {
+                    role: "cut",
+                    accelerator: "CmdOrCtrl+X"
+                },
+                {
+                    role: "copy",
+                    accelerator: "CmdOrCtrl+C"
+                },
+                {
+                    role: "paste",
+                    accelerator: "CmdOrCtrl+V"
+                },
+                {
+                    role: "delete",
+                    accelerator: "Delete"
+                },
+                { type: "separator" },
+                {
+                    role: "selectAll",
+                    accelerator: "CmdOrCtrl+A"
+                }
+            ]
+        },
+        {
+            label: "View",
+            submenu: [
+                {
+                    role: "zoomin",
+                    accelerator: "CmdOrCtrl+="
+                },
+                {
+                    role: "zoomout",
+                    accelerator: "CmdOrCtrl+-"
+                },
+                {
+                    label: "Reset Zoom",
+                    role: "resetzoom",
+                    accelerator: "CmdOrCtrl+0"
+                }
+            ]
+        }
+    ]);
+    Menu.setApplicationMenu(menu);
 
     // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
+    // win.webContents.openDevTools()
 
     // Emitted when the window is closed.
-    mainWindow.on("closed", function() {
+    win.on("closed", function() {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
-        mainWindow = null;
+        win = null;
     });
 }
 
@@ -45,7 +140,7 @@ app.on("window-all-closed", function() {
 app.on("activate", function() {
     // On OS X it"s common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
+    if (win === null) {
         createWindow();
     }
 });
